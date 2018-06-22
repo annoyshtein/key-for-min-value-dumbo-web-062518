e6aeab687fdd430f6c53702b00886c39637a6b11
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 03:56:58 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   name_hash[:compare] = Float::INFINITY
   the_value = nil
   name_hash.collect do |key, value|
-    name_nash[:chair] < name_hash[key]
+    name_hash[:chair] < name_hash[key]
   end
   #puts the_value
   puts name_hash
