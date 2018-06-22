6abe4b6a754966b2e5aa697f5a5ca076c117c418
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 03:43:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   #name_hash.collect do |key, value|
     
   #end
-  puts the_value
+  #puts the_value
 end
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}
