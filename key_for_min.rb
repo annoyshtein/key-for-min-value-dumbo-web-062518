73f8ba3d83daf36c5a57cc0e5771df21f6fe0af6
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 04:01:57 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     if name_hash[key] < lowest_found_value 
       puts name_hash[key].to_s + " is lower than " + lowest_found_value.to_s
+      lowest_found_value = 
     end
   end
   #puts the_value
