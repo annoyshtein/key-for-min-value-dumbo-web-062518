ad4b2ba3b45a6c930f037592eb25a9a9686723f6
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 04:21:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,13 +8,13 @@ def key_for_min_value(name_hash)
     if name_hash[key] < lowest_found_value 
       lowest_found_value = name_hash[key]
       lowest_found_key = key
-      puts lowest_found_key
+      # puts lowest_found_key
       return lowest_found_key
     end
   end
 end
 
-ikea = {:chair => 85, :table => 830, :mattress => 45}
+# ikea = {:chair => 85, :table => 830, :mattress => 45}
 
-key_for_min_value(ikea)
+# key_for_min_value(ikea)
 
