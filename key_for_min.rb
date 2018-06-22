1031e947dba2822dd3ebc414c1a63e17feb7ce17
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 03:50:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash[:compare] = infinity
+  name_hash[:compare] = INFINITY
   name_hash.collect do |key, value|
     puts name_hash[:compare]
     puts name_hash[key]
