89acfd2302ee1dbe0b227b14f0554069ac9dde05
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 04:23:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,8 @@ def key_for_min_value(name_hash)
     if name_hash[key] < lowest_found_value 
       lowest_found_value = name_hash[key]
       lowest_found_key = key
-      # puts lowest_found_key
-      return lowest_found_key
+      puts lowest_found_key
+      # return lowest_found_key
     end
   end
 end
