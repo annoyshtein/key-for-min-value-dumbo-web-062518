e2c6168dd9fbafff6ffc282f04558cc6663871ab
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 04:14:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   lowest_found_value = Float::INFINITY
   # the_value = nil
-  name_hash.collect do |value, key|
+  name_hash.collect do |key, value|
     if name_hash[value] < lowest_found_value 
-      puts key.to_s
+      puts .to_s
       puts name_hash[value].to_s + " is lower than " + lowest_found_value.to_s
       lowest_found_value = name_hash[value]
       puts name_hash[value].to_s + " is the new lowest found value. The corresponding key is " + name_hash[key].to_s
