4affe12a28725895e7fa9ad45e901f41143e1866
Alex Neustein <alex.neustein@gmail.com>
2018-06-22 04:15:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   # the_value = nil
   name_hash.collect do |key, value|
     if name_hash[key] < lowest_found_value 
-      puts value.to_s
+      puts key.to_s
       puts name_hash[key].to_s + " is lower than " + lowest_found_value.to_s
       lowest_found_value = name_hash[value]
       puts name_hash[key].to_s + " is the new lowest found value. The corresponding key is " + name_hash[key].to_s
